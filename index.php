e843c94f179560408dd671f47415752081aa83e9
Nur Ahmad Dzulkarim <nadzulkarim@gmail.com>
2022-10-24 10:56:20 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+echo "Hello World!<br>";
+
+$nama = "Nur Ahmad Dzulkarim";
+$umur = 22;
+
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Amalia";
+$umurKakak = 26;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihUmur = $umurKakak - $umur;
+echo "Selisih usia saya dan kakak saya adalah $selisihUmur tahun."
+
+?>
